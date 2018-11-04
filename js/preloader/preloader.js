afc509ab2d2836cb7eeb2cda9b25ea315a852e4f
ArturAleshchyk <artur.aleshchyk@gmail.com>
2018-11-04 19:30:39 +0300

    Add preloader and temporary logo
    Preloader on this stage only works with loading images. Need some isues to calculate all objects from webpage

--- js/preloader/preloader.js
+++ js/preloader/preloader.js
@@ -1,9 +1,25 @@
-document.body.onload = function () {
-  setTimeout(() => {
-    const preloader = document.getElementById('page-preloader');
+var images = document.images;
+var imagesTotalCount = images.length;
+var preloader = document.getElementById('page-preloader');
+var imagesLoadedCount = 0;
+var percDisplay = document.getElementById('load-perc');
 
-    if (!preloader.classList.contains('done')) {
-      preloader.classList.add('done');
-    }
-  }, 1000);
-};
+for (var i = 0; i < imagesTotalCount; i++) {
+  var imageClone = new Image();
+  imageClone.onload = loadImages;
+  imageClone.onerror = loadImages;
+  imageClone.src = images[i].src;
+}
+
+function loadImages() {
+  imagesLoadedCount++;
+  percDisplay.innerHTML = (((100 / imagesTotalCount) * imagesLoadedCount) << 0) + '%';
+
+  if (imagesLoadedCount >= imagesTotalCount) {
+    setTimeout(function () {
+      if (!preloader.classList.contains('done')) {
+        preloader.classList.add('done');
+      }
+    }, 1000);
+  }
+}
