d4307c919327f7929eea1cd4755821b635d01bdf
Artur Aleshchyk <aleshchyk.artur@gmail.com>
2018-11-18 14:17:46 +0200

    Add local storage for bookmark name and url

--- js/bookmarks/bookmarks.js
+++ js/bookmarks/bookmarks.js
@@ -1,10 +1,30 @@
 document.getElementById('myForm').addEventListener('submit', saveBookmark);
+
 function saveBookmark(eo) {
   //Get form values
   const siteName = document.getElementById('siteName').value;
   const siteUrl = document.getElementById('siteUrl').value;
 
-  console.log(siteName + ' ' + siteUrl);
+  const bookmark = {
+    name: siteName,
+    url: siteUrl,
+  };
+
+  if (localStorage.getItem('bookmarks') === null) {
+    //init array
+    let bookmarks = [];
+    //Add to array
+    bookmarks.push(bookmark);
+    //Set to local storage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  } else {
+    //Get bookmarks from local storage
+    let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    //Add bookmark to array
+    bookmarks.push(bookmark);
+    //Rewrite local storage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
 
   //Prevent form from submitting
   eo.preventDefault();
