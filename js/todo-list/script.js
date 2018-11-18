10ad3fcbbc284c558aa992860f2fee48d1b896fd
Artur Aleshchyk <aleshchyk.artur@gmail.com>
2018-11-18 11:48:37 +0200

    Codestyle refactor

--- js/todo-list/script.js
+++ js/todo-list/script.js
@@ -1,27 +1,33 @@
-var list = document.querySelector('ul');
-list.addEventListener('click', function (ev) {
+const list = document.querySelector('ul');
+
+list.addEventListener('click', (ev) => {
   if (ev.target.tagName === "LI") {
     ev.target.classList.toggle('text-danger');
   } else if (ev.target.tagName === "BUTTON") {
-    var div = ev.target.parentNode;
+    const div = ev.target.parentNode;
     div.remove();
   }
 }, false);
 
 function newElement() {
-  var li = document.createElement('li');
-  var inputValue = document.getElementById('toDoEl').value;
-  var t = document.createElement('SPAN');
+  const li = document.createElement('li');
+  const inputValue = document.getElementById('toDoEl').value;
+  const t = document.createElement('SPAN');
+
   t.innerHTML = inputValue;
   li.appendChild(t);
+
   if (inputValue == "") {
     alert("Введите ваше дело!");
   } else {
     document.getElementById('list').appendChild(li);
   }
+
   document.getElementById('toDoEl').value = "";
-  var span = document.createElement('BUTTON');
-  var txt = document.createTextNode("DELETE");
+
+  const span = document.createElement('BUTTON');
+  const txt = document.createTextNode("DELETE");
+
   span.className = "btn-danger";
   span.appendChild(txt);
   li.appendChild(span);
