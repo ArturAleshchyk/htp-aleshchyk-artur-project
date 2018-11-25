d1b1fa78fd69bad16b5af087395d8ea5cd2d7bd6
Aleshchyk Artur <aleshchyk.artur@gmail.com>
2018-11-25 15:21:04 +0200

    Add inner canvas preloader variables

---
+++ js/preloader/preloaderCanvas.js
@@ -0,0 +1,11 @@
+var canvas = document.getElementById('canvas');
+var c = canvas.getContext('2d');
+
+var innerWidth = window.innerWidth;
+var innerHeight = window.innerHeight;
+var doublePi = Math.PI * 2;
+var circleArray = [];
+var numOfCircles = 100;
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
