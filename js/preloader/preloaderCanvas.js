4ca6ed6454e1fee161263aab5db5ba6f4a9bc4d6
Aleshchyk Artur <aleshchyk.artur@gmail.com>
2018-11-25 15:46:06 +0200

    Add functions to create and animate circles

--- js/preloader/preloaderCanvas.js
+++ js/preloader/preloaderCanvas.js
@@ -9,3 +9,36 @@ var numOfCircles = 100;
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
+
+//Function constr. for new circles
+function Circle(x, y, dx, dy, radius) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.color = "#fff";
+  this.radius = radius;
+
+  c.beginPath();
+  c.arc(this.x, this.y, this.radius, doublePi,false);
+  c.fillStyle = this.color;
+  c.fill();
+  c.closePath();
+}
+
+//Creating new circles
+var radius = (Math.random() *30) + 1;
+var x = Math.random() * (innerWidth - radius * 2) +radius;
+var y = Math.random() * (innerHeight - radius * 2) +radius;
+var dx = Math.random() - 0.5 ;
+var dy = Math.random() - 0.5;
+
+//Function to animate circles
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0,0, innerWidth, innerHeight)
+
+  new Circle(x, y, dx, dy, radius);
+}
+
+animate();
