ae4c24f12888d4f8b8c67ea30607c26817fa2779
Aleshchyk Artur <aleshchyk.artur@gmail.com>
2018-11-25 16:03:23 +0200

    Fix bug with circle start position

--- js/preloader/preloaderCanvas.js
+++ js/preloader/preloaderCanvas.js
@@ -19,17 +19,16 @@ function Circle(x, y, dx, dy, radius) {
   this.color = "#fff";
   this.radius = radius;
 
-  //Update circles statemant function
+  //Update circles position function
   this.update = function () {
     if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
       this.dx -= this.dx;
-    }
-    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
-      this.dx -= this.dx;
+    } else if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+      this.dy -= this.dy;
     }
 
     this.x += this.dx;
-    this.y = this.dy;
+    this.y += this.dy;
 
     this.draw();
   };
