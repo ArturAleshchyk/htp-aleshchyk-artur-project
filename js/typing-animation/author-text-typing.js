a001634832476b5ed46818ac8fcd845b20989031
Artur <aleshchyk.artur@gmail.com>
2018-11-28 19:00:47 +0300

    Fix bug with typing animation

--- js/typing-animation/author-text-typing.js
+++ js/typing-animation/author-text-typing.js
@@ -1,8 +1,9 @@
+'use strict';
+
 var i = 0;
 var authorInfo = `Created by Aleshchyk Artur - GitHub https://github.com/ArturAleshchyk.`;
 
-
-letsTipe = setTimeout(function typingSlogan() {
+var letsTipe = setTimeout(function typingSlogan() {
   if (i < authorInfo.length) {
     document.getElementById('author-info').innerHTML += authorInfo.charAt(i);
     i++;
